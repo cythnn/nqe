cb9b1ea9cfb940a9f5f82886260e867c8c759ca9
Jeroen Vuurens <jbpvuurens@gmail.com>
2016-05-02 17:45:49 +0200

    selectwords

--- nqe/findmax.pyx
+++ nqe/findmax.pyx
@@ -1,5 +1,5 @@
+from __future__ import print_function 
 import struct
-
 import fcntl, os
 from numpy import zeros, float32, int32, dtype, fromstring
 from tools.types cimport *
@@ -16,13 +16,13 @@ def loadvectors(fname, length=None, filter=None):
         table = zeros((vocab_size, vector_size), dtype=float32)
 
         def add_word(word, weights):
-            if len(word2vec) % 100 == 0:
+            if len(word2vec) % 10 == 0:
                 print("words", len(word2vec))
             word_id = len(word2vec)
             if word in word2vec:
                 print("duplicate word '%s' in %s, ignoring all but first", word, fname)
                 return
-            #print("add_word", word, word_id)
+            print("add_word", word, word_id)
             table[word_id] = weights
             word2vec[word] = word_id
             vec2word[word_id] = word
@@ -33,27 +33,29 @@ def loadvectors(fname, length=None, filter=None):
             word = []
             while True:
                 ch = fin.read(1)
-                if ch == '':
+                if ch == b'':
                     success = False
                     break
                 elif ch == b' ':
                     break
                 if ch != b'\n':
                     word.append(ch)
-            word = b''.join(word)
-            if filter is None or word in filter:
+            word = (b''.join(word)).decode(encoding="UTF-8")
+            if filter is None or word.lower() in filter:
                 weights = fromstring(fin.read(binary_len), dtype=float32)
                 if (len(weights) < 300):
+                    print("too small")
                     break
                 add_word(word, weights)
             else:
                 fin.read(binary_len)
+        table.resize((len(word2vec), vector_size))
     return table, word2vec, vec2word
 
 def save_binary(fname, word2vec, array):
     with open(fname, 'wb') as fout:
         fout.write(("%s %s\n" % (array.shape[0], array.shape[1])).encode())
-        for word, index in word2vec:
+        for word, index in word2vec.items():
             fout.write((word + " ").encode())
             fout.write(struct.pack('%sf' % array.shape[1], *array[index]))
 
